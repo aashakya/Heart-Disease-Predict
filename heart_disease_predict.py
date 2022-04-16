934c0faf21582239705424cffea484dda5289ba1
aashakya <aakrista.shakya1@gmail.com>
2022-04-16 01:55:30 -0500

    Add data + feature eng instruction

---
+++ heart_disease_predict.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 01:12:01 2022
+
+@author: ashakya
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("heart_2020_cleaned.csv")
+
+# Age category to numerical range 0-12
+# GenHealth categorical to numerical
+# Smoking categorical to numerical
+# AlcoholDrinking categorical to numerical
+# Stroke categorical to numerical
+# DiffWalking categorical to numerical
+# Asthma categorical to numerical
+# KidneyDisease categorical to numerical
+# Diabetic categorical to numerical
+# PhysicalActivity to numerical
+# SkinCancer categorical to numerical
+# Race categorical to numerical
+# Sex categorical to numerical
+# Normalize BMI
+# Normalize SleepTime
+# Normalize Physical Health
+# Normalize Mental Health
